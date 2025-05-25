8bb5f2cd64662d9792867cb781a165b7dbed559f
Mohamed Reda <moo.galaxy.pc@gmail.com>
2025-05-25 19:12:08 +0300

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,4 @@
+print ("Welcom to the YouTube channekl name Generator.")
+name = input ("What is your nickname: ").capitalize()
+channle_about = input ("What is your channel about: ").capitalize()
+print (f"You could name your channle '{channle_about} with {name}'")
